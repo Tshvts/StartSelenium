247bce51681348c94ac248fe60f51e7356030242
tdshe <tnshvts1602@gmail.com>
2024-11-21 18:13:08 +0200

    HW

--- src/test/java/tests/SelectorCSS.java
+++ src/test/java/tests/SelectorCSS.java
@@ -1,4 +1,92 @@
 package tests;
 
-public class SelectorCSS {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SelectorCSS
+{
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void elementsHeaderIlCarro()
+    {
+        driver.navigate().to("https://ilcarro.web.app/");
+        driver.manage().window().maximize();
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement linkCar = driver.findElement(By.cssSelector("a[href='/let-car-work']"));
+        linkCar.click();
+        System.out.println("Text: " + linkCar.getText());
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement linkSearch = driver.findElement(By.cssSelector("a[href='/search']"));
+        linkSearch.click();
+        System.out.println("Text: " + linkSearch.getText());
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement linkTerms= driver.findElement(By.cssSelector("a[href='/terms-of-use']"));
+        linkTerms.click();
+        System.out.println("Text: " + linkTerms.getText());
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement linkSignUp= driver.findElement(By.cssSelector("a[href='/registration?url=%2Fterms-of-use']"));
+        linkSignUp.click();
+        System.out.println("Text: " + linkSignUp.getText());
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement linkLogIn= driver.findElement(By.cssSelector("a[href='/login?url=%2Fterms-of-use']"));
+        linkLogIn.click();
+        System.out.println("Text: " + linkLogIn.getText());
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement imgLogo = driver.findElement(By.cssSelector("img[alt='logo']"));
+        imgLogo.click();
+
+        driver.quit();
+    }
 }
--- src/test/java/tests/StartTests.java
+++ src/test/java/tests/StartTests.java
@@ -1,4 +1,42 @@
 package tests;
 
-public class StartTests {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StartTests
+{
+     //System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void firstTest()
+    {
+        driver.navigate().to("https://telranedu.web.app/home");
+        try
+        {
+            Thread.sleep(2000);
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        WebElement btnLoginHeader = driver.findElement(By.cssSelector("a[href='/login']"));
+        btnLoginHeader.click();
+        WebElement btnRegistration = driver.findElement(By.cssSelector("button[name='registration']"));
+        btnRegistration.click();
+
+    }
+
+    @Test
+    public void secondTest()
+    {
+        driver.navigate().to("https://telranedu.web.app/home");
+        WebElement elementText = driver.findElement(By.cssSelector("h2"));
+        System.out.println(elementText.getText());
+        WebElement btnAbout = driver.findElement(By.cssSelector("*[href='/about']"));
+        System.out.println("Tage name: " + btnAbout.getTagName());
+        System.out.println("Text: " + btnAbout.getText());
+    }
 }
