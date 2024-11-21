92366f8de72cceca744d674bf709a6a15933f704
tdshe <tnshvts1602@gmail.com>
2024-11-21 18:17:20 +0200

    HW

--- src/test/java/tests/selectorCSS.java
+++ src/test/java/tests/selectorCSS.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class SelectorCSS
+public class selectorCSS
 {
     WebDriver driver = new ChromeDriver();
 
